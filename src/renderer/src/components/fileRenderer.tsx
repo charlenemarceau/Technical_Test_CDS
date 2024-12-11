7ea64341ffd830d74d3879495ac3cb5a33b86bb0
Charlène <charlenemarceau@hotmail.fr>
2024-12-11 13:39:56 +0100

    three js canva upload wip

--- src/renderer/src/components/fileRenderer.tsx
+++ src/renderer/src/components/fileRenderer.tsx
@@ -3,232 +3,155 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
-import GUI from 'lil-gui';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import React, { useEffect, useRef } from 'react';
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const FileRenderer = ({ setFileData }) => {
+  // Référence pour le canvas
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-/**
- * LIL GUI - DEBUG
- */
-// const gui = new GUI({
-//   width: 300,
-//   title: 'Options',
-//   closeFolders: false,
-// });
+  useEffect(() => {
+    if (!canvasRef.current) {
+      console.error('Canvas non trouvé.');
+      return;
+    }
 
-// gui.add(options, 'mode', ['Normal', 'Wireframe', 'Shaded']).name('Mode');
-// gui.add(model.scale, 'x', 0.1, 5).name('Scale X');
-//   gui.add(model.scale, 'y', 0.1, 5).name('Scale Y');
-//   gui.add(model.scale, 'z', 0.1, 5).name('Scale Z');
-// Clipping plane : un plan horizontal initial
-// const plane = new THREE.Plane(new THREE.Vector3(0, -1, 0), 0); // Orientation Y descendante
-// renderer.localClippingEnabled = true; // Activer les clipping planes
-// const planeFolder = gui.addFolder('Clipping Plane');
-//     const planeParams = {
-//       constant: 0, // Décale le plan
-//       x: 0, // Orientation X
-//       y: -1, // Orientation Y
-//       z: 0, // Orientation Z
-//     };
+    // Initialisation de la scène
+    const scene = new THREE.Scene();
+    const gltfLoader = new GLTFLoader()
 
-//     planeFolder.add(planeParams, 'constant', -1, 1, 0.01).name('Plane Offset').onChange((value) => {
-//       plane.constant = value;
-//     });
 
-//     planeFolder.add(planeParams, 'x', -1, 1, 0.01).name('Plane Normal X').onChange((value) => {
-//       plane.normal.x = value;
-//       plane.normal.normalize(); // Normaliser pour éviter les artefacts
-//     });
+    gltfLoader.load(
+      setFileData.path,
+      (gltf) =>
+      {
+          gltf.scene.scale.set(0.025, 0.025, 0.025)
+          scene.add(gltf.scene)
 
-//     planeFolder.add(planeParams, 'y', -1, 1, 0.01).name('Plane Normal Y').onChange((value) => {
-//       plane.normal.y = value;
-//       plane.normal.normalize();
-//     });
+          // Animation
+          // mixer = new THREE.AnimationMixer(gltf.scene)
+          // const action = mixer.clipAction(gltf.animations[2])
+          // action.play()
+      }
+  )
 
-//     planeFolder.add(planeParams, 'z', -1, 1, 0.01).name('Plane Normal Z').onChange((value) => {
-//       plane.normal.z = value;
-//       plane.normal.normalize();
-//     });
+    /**
+     * Floor
+     */
+    const floor = new THREE.Mesh(
+      new THREE.PlaneGeometry(10, 10),
+      new THREE.MeshStandardMaterial({
+          color: '#444444',
+          metalness: 0,
+          roughness: 0.5
+      })
+    )
+    floor.receiveShadow = true
+    floor.rotation.x = - Math.PI * 0.5
+    scene.add(floor)
 
-//     planeFolder.open();
-// Canvas
-const canvas = document.querySelector('canvas.webgl');
+    /**
+    * Lights
+    */
+    const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+    scene.add(ambientLight)
 
-// Scene
-const scene = new THREE.Scene();
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+    directionalLight.castShadow = true
+    directionalLight.shadow.mapSize.set(1024, 1024)
+    directionalLight.shadow.camera.far = 15
+    directionalLight.shadow.camera.left = - 7
+    directionalLight.shadow.camera.top = 7
+    directionalLight.shadow.camera.right = 7
+    directionalLight.shadow.camera.bottom = - 7
+    directionalLight.position.set(- 5, 5, 0)
+    scene.add(directionalLight)
 
-// ----------------------------------------------------------------
-// FIRST SCENE ----------------------------------------------------------------
-//object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: 'red'});
-const mesh = new THREE.Mesh(geometry, material);
-//position
-mesh.position.x = 0.7;
-mesh.position.y = -0.6;
-mesh.position.z = 1;
+    // Axes Helper
+    const axesHelper = new THREE.AxesHelper(2);
+    scene.add(axesHelper);
 
-mesh.position.set(0.7, -0.6, 1)
-scene.add(mesh);
+    // Taille du canvas
+    const sizes = {
+      width: (window.innerWidth / 3) * 2,
+      height: (window.innerWidth / 3) * 2,
+    };
 
-//Scale
-// mesh.scale.set(0.5, 0.5, 0.5);
-mesh.scale.x = 2;
-mesh.scale.y = 0.5;
-mesh.scale.z = 0.5;
+    // Caméra
+    // Base camera
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+    camera.position.set(3, 3, 3)
+    scene.add(camera);
 
-//Rotation : updatating one will update the other. Have to be careful with the order of rotation. Gimbal Lock.
-//Quaternion: expresses a rotationbut in a more mathematical way.
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25;
-mesh.rotation.y = Math.PI * 0.25;
+    // Controls
+    const controls = new OrbitControls(camera, canvasRef.current)
+    controls.target.set(0, 0.75, 0)
+    controls.enableDamping = true
 
-// let mixer = null;
-// const gltfLoader = new GLTFLoader()
-// // if (setFileData) {
-//   console.log('setFileData.path', setFileData.path)
-
-//   gltfLoader.load(
-//     setFileData.path,
-//     (gltf) =>
-//       {
-//         console.log('glTF', gltf)
-//         gltf.scene.scale.set(0.025, 0.025, 0.025)
-//         window.onload = (e) => {
-//         scene.add(gltf.scene)
-//       }
-
-//         // Animation
-//         // mixer = new THREE.AnimationMixer(gltf.scene)
-//         // const action = mixer.clipAction(gltf.animations[2])
-//         // action.play()
-//     },
-//     undefined,
-//     (error) => {
-//         console.error('Erreur de chargement GLTF:', error);
-//     }
-//   )
-
-/**
- * Floor
- */
-const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(10, 10),
-  new THREE.MeshStandardMaterial({
-      color: '#444444',
-      metalness: 0,
-      roughness: 0.5
-  })
-)
-floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
-scene.add(floor)
-
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
-scene.add(ambientLight)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(- 5, 5, 0)
-scene.add(directionalLight)
-
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight
-}
-window.onload = (e) => {
-  window.addEventListener('resize', () =>
-  {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
+    // Renderer
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvasRef.current,
+      antialias: true,
+    });
+    renderer.shadowMap.enabled = true
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-  })
-  /**
-   * Camera
-  */
- // Base camera
- const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
- camera.position.set(2, 2, 2)
- scene.add(camera)
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0.75, 0)
-controls.enableDamping = true
+    const clock = new THREE.Clock()
+    let previousTime = 0
 
-/**
-* Renderer
-*/
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
+    const tick = () =>
+      {
+          const elapsedTime = clock.getElapsedTime()
+          // const deltaTime = elapsedTime - previousTime
+          previousTime = elapsedTime
 
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+          // Model animation
+          // if(mixer)
+          // {
+          //     mixer.update(deltaTime)
+          // }
 
-/**
- * Animate
-*/
-const clock = new THREE.Clock()
-let previousTime = 0
+          // Update controls
+          // controls.update()
 
-const tick = () =>
-  {
-    const elapsedTime = clock.getElapsedTime()
-    previousTime = elapsedTime
+          // Render
+          renderer.render(scene, camera)
 
-    // Model animation
-    // if(mixer)
-    // {
-    //     mixer.update(deltaTime)
-    // }
+          // Call tick again on the next frame
+          window.requestAnimationFrame(tick)
+      }
 
-    // Update controls
-    controls.update()
+      tick();
 
-    // Render
-    renderer.render(scene, camera)
+    // Gestion du redimensionnement
+    const handleResize = () => {
+      sizes.width = (window.innerWidth / 3) * 2;
+      sizes.height = (window.innerWidth / 3) * 2;
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
 
-tick()
-}
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    };
 
+    window.addEventListener('resize', handleResize);
+
+    // Nettoyage lors du démontage du composant
+    return () => {
+      window.removeEventListener('resize', handleResize);
+      renderer.dispose();
+    };
+  }, []);
   return (
     <div id="canva">
       <div className="canva_content">
         <h2 id="file_name">{setFileData.name}</h2>
         <div className="canvas_content">
-          <canvas className="webgl"></canvas>
+          <canvas ref={canvasRef} className="webgl"></canvas>
           <div className="informations_content">
             <h3>Informations</h3>
             <ul>
